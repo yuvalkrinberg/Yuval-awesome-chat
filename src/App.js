c2ae86240d08dd4b791ee5ec03195760328eae3c
yuvalkrin@gmail.com <yuval123654>
2020-06-01 22:53:26 +0300

    app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from '@livechat/ui-kit';
+import Chat from './components/chat';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => (
+  <ThemeProvider>
+    <Chat />
+  </ThemeProvider>
+);
 
 export default App;
